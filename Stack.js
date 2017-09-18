7956c9a2521917dc739525f36bfe6ed544377145
dravenxiaokai <jspankai@vip.qq.com>
2017-09-18 21:19:47 +0800

    栈

--- Stack.js
+++ Stack.js
@@ -101,3 +101,26 @@ if (isPalindrome(word)) {
 } else {
     console.log(word + ' is not a palindrome.')
 }
+
+function factorial(n) {
+    if (n === 0) {
+        return 1
+    } else {
+        return n * factorial(n - 1)
+    }
+}
+
+function fact(n) {
+    var s = new Stack()
+    while (n > 1) {
+        s.push(n--)
+    }
+    var product = 1;
+    while (s.length() > 0) {
+        product *= s.pop()
+    }
+    return product
+}
+
+console.log(factorial(5))   // 120
+console.log(fact(5))    // 120
